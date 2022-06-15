cc8264814a26fdac35781d6969232809e36147c0
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-06-15 04:07:43 -0700

    shekoofeh-shojai

--- redux-project/redux-project/src/index.js
+++ redux-project/redux-project/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import App from './App';
 import store from './redux/store';
 import { Provider } from 'react-redux';
@@ -8,8 +8,8 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode store={store}>
-    <Provider>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
--- redux-project/redux-project/src/redux/redusers/productReduser.js
+++ redux-project/redux-project/src/redux/redusers/productReduser.js
@@ -1,5 +1,5 @@
 import { type } from "@testing-library/user-event/dist/type";
-
+import { ActionTypes } from "../continers/action-types";
 const intialState={
     procucts:[{
 
